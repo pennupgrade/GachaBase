e76b12f17bff1bc743cf77b2a2f3aab1daca45db
Anthony Ge <geant@seas.upenn.edu>
2024-07-30 21:37:50 -0400

    A pity system technically exists

--- Assets/Scripts/GachaChest.cs
+++ Assets/Scripts/GachaChest.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class GachaChest : MonoBehaviour
 {
@@ -26,7 +27,10 @@ public class GachaChest : MonoBehaviour
     public float clickScaleFactor;
     private Vector3 targetScale;
 
-    
+    // Pity system vars
+    private int rollSinceLastUnique;
+    private int pityThreshold = 3;
+    private float pityPercentageUp;
 
     private bool mousedOver;
 
@@ -117,29 +121,68 @@ public class GachaChest : MonoBehaviour
     //Spits out a Random Monster from the bank
     public void Roll()
     {
+        rollSinceLastUnique += 1; // use this for our pity system later
+        pityPercentageUp += 0.05f;
+
+        List<MonsterAsset> uncollectedMonsters = new List<MonsterAsset>();
+
+        foreach (var monster in InventoryManager.Instance.Monsters)
+        {
+            if (!InventoryManager.Instance.Owned[monster.Name])
+            {
+                uncollectedMonsters.Add(monster);
+            }
+        }
+        
+        List<MonsterAsset> Pool = InventoryManager.Instance.Monsters;
+
+        // pity system
+        bool isPityRoll = false;
+        float numUniqueRatio = 1.0f - (uncollectedMonsters.Count / Pool.Count);
+        numUniqueRatio = numUniqueRatio > 0.5 ? 0.0f : numUniqueRatio / 12.0f;
+        float randomNumber = Random.Range(0.0f, 1.0f);
+        float pityPercentageThreshold = Mathf.Clamp(pityPercentageUp + numUniqueRatio, 0.0f, 0.4f);
+
+        Debug.Log(
+            "Pity system stats: " + rollSinceLastUnique + 
+            ", Threshold: " + pityThreshold + "; rand: " + randomNumber + 
+            " | " + pityPercentageThreshold);
+
+        if (rollSinceLastUnique > pityThreshold || randomNumber < pityPercentageThreshold)
+        {
+            rollSinceLastUnique = 0;
+            pityPercentageUp = 0;
+            isPityRoll = true;
+            Pool = uncollectedMonsters; // guaranteed unique monster pulled
+        }
 
         //Fuck with probabilities here for rarer items hehe
-        int roll = Random.Range(0, numOfMonsters);
+        int roll = Random.Range(0, Pool.Count);
         Debug.Log("You rolled a: " + roll);
 
         //Update the player's inventory directly from here
 
-        MonsterAsset pulledMonster = InventoryManager.Instance.Monsters[roll];
+        MonsterAsset pulledMonster = Pool[roll];
         string pullName = pulledMonster.Name;
         ShowCard(pulledMonster.IconSprite);
+
         if (!InventoryManager.Instance.Owned[pullName])
         {
+            if (!isPityRoll) // in the event we get a unique but it wasn't from a pity roll
+            {
+                rollSinceLastUnique = 0;
+                pityPercentageUp = 0;
+            }
+
             Debug.Log("You pulled a: " + pullName);
             InventoryManager.Instance.Pull(pullName);
-            //InventoryManager.Instance.Owned[pullName] = true;
-            
-            //GUIBehavior.instance.updateDropdown();
         } 
         else
         {
             Debug.Log("You already have a " + pullName);
         }
     }
+
 
     public void ShowCard(Sprite s) {
         cardImage.GetComponent<Image>().sprite = s;

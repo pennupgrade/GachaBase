e4f6b99ae1e53a8ce0907cdcfa05cb3b077d1549
Charles Wang <czw@seas.upenn.edu>
2024-08-06 21:32:02 -0700

    feat: finish

--- Assets/Scripts/ControlsPopup.cs
+++ Assets/Scripts/ControlsPopup.cs
@@ -15,7 +15,10 @@ internal enum ControlType
 public class ControlsPopup : MonoBehaviour
 {
     [SerializeField] private ControlType controlType;
-    [SerializeField] private RectTransform overlay;
+    
+    [SerializeField] private GameObject overlay;
+    private RectTransform overlayRect;
+    private Image overlayImage;
     
     [SerializeField] private GameObject imageSpace;
     private Image space;
@@ -32,6 +35,10 @@ public class ControlsPopup : MonoBehaviour
     [SerializeField] [Min(0f)] private float slideInAnimationDuration;
     [SerializeField] [Min(0f)] private float opacityAnimationDuration;
 
+    private float duration;
+    private bool showInput;
+    private bool shownPopup;
+    
     private void Awake() {
         space = imageSpace.GetComponent<Image>();
         wasd = imageWasd.GetComponent<Image>();
@@ -44,31 +51,71 @@ public class ControlsPopup : MonoBehaviour
         tmp = controlsText.GetComponent<TMP_Text>();
         tmp.color = Color.clear;
 
-        overlay.anchoredPosition = new Vector2(-2100f, overlay.anchoredPosition.y);
+        overlayRect = overlay.GetComponent<RectTransform>();
+        overlayImage = overlay.GetComponent<Image>();
+        overlayRect.anchoredPosition = new Vector2(-2100f, overlayRect.anchoredPosition.y);
+        
         space.color = Color.clear;
         wasd.color = Color.clear;
         mouse.color = Color.clear;
+
+        duration = 0f;
+        showInput = true;
+        shownPopup = false;
     }
 
-    private void Start() {
-        switch (controlType) {
-            case ControlType.Space:
-                imageSpace.SetActive(true);
-                break;
-            
-            case ControlType.Wasd:
-                imageWasd.SetActive(true);
-                break;
-            
-            case ControlType.Mouse:
-                imageMouse.SetActive(true);
-                break;
-            
-            default:
-                throw new ArgumentOutOfRangeException();
+    private void Update() {
+        duration += Time.deltaTime;
+
+        if (duration <= 3f) {
+            if (CheckControlInput()) {
+                showInput = false;
+            }
         }
+        else {
+            if (showInput) {
+                switch (controlType) {
+                    case ControlType.Space:
+                        imageSpace.SetActive(true);
+                        break;
+            
+                    case ControlType.Wasd:
+                        imageWasd.SetActive(true);
+                        break;
+            
+                    case ControlType.Mouse:
+                        imageMouse.SetActive(true);
+                        break;
+            
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
         
-        StartCoroutine(AnimateOpacity());
+                StartCoroutine(AnimateOpacity());
+                showInput = false;
+                shownPopup = true;
+            }
+        }
+
+        if (shownPopup && CheckControlInput()) {
+            StartCoroutine(AnimateFadeOut());
+            shownPopup = false;
+        }
+    }
+
+    private bool CheckControlInput() {
+        return controlType switch {
+            ControlType.Space => Input.GetKeyDown(KeyCode.Space),
+            
+            ControlType.Wasd => Input.GetKeyDown(KeyCode.W) || 
+                                Input.GetKeyDown(KeyCode.A) ||
+                                Input.GetKeyDown(KeyCode.S) || 
+                                Input.GetKeyDown(KeyCode.D),
+            
+            ControlType.Mouse => Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1),
+            
+            _ => throw new ArgumentOutOfRangeException()
+        };
     }
 
     private IEnumerator AnimateOpacity() {
@@ -100,16 +147,38 @@ public class ControlsPopup : MonoBehaviour
         }
     }
 
+    private IEnumerator AnimateFadeOut() {
+        var progress = 0f;
+        const float CURR_DURATION = 0.3f;
+
+        var beginOverlayColor = overlayImage.color;
+        
+        while (progress <= CURR_DURATION) {
+            progress += Time.deltaTime;
+            var t = progress / CURR_DURATION;
+
+            overlayImage.color = Color.Lerp(beginOverlayColor, Color.clear, t);
+            space.color = Color.Lerp(Color.white, Color.clear, t);
+            wasd.color = Color.Lerp(Color.white, Color.clear, t);
+            mouse.color = Color.Lerp(Color.white, Color.clear, t);
+            tmp.color = Color.Lerp(Color.white, Color.clear, t);
+            
+            yield return null;
+        }
+        
+        overlay.SetActive(false);
+    }
+
     private IEnumerator AnimateSlideIn() {
-        var before = new Vector2(-2100f, overlay.anchoredPosition.y);
-        var after = new Vector2(0f, overlay.anchoredPosition.y);
+        var before = new Vector2(-2100f, overlayRect.anchoredPosition.y);
+        var after = new Vector2(0f, overlayRect.anchoredPosition.y);
 
         var progress = 0f;
         while (progress <= slideInAnimationDuration) {
             progress += Time.deltaTime;
-            overlay.anchoredPosition = Vector2.Lerp(before, 
-                                                    after, 
-                                                    Easing.OutQuad(progress / slideInAnimationDuration));
+            overlayRect.anchoredPosition = Vector2.Lerp(before, 
+                                                        after, 
+                                                        Easing.OutQuad(progress / slideInAnimationDuration));
 
             yield return null;
         }
